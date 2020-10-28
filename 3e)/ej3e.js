9a22aaeafe41dbe8d6b2235153c9506a3e01d3f8
FedericoB <federicob@gpl.com.ar>
2020-10-27 21:31:26 -0300

    ejercicio 3 E

---
+++ 3e)/ej3e.js
@@ -0,0 +1,7 @@
+/* Invertir el orden del array (utilizar reverse). */
+
+var months = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 
+'Octubre', 'Noviembre', 'Diciembre'];
+months.reverse();
+
+//console.log(months)

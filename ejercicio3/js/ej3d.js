f204ec93405cfdc2c5ac2aea9471db2e37fcf0d7
FedericoB <federicob@gpl.com.ar>
2020-10-27 23:02:34 -0300

    ejercicio 3

---
+++ ejercicio3/js/ej3d.js
@@ -0,0 +1,8 @@
+/* Quitar un elemento del principio y del final del array (utilizar shift y pop). */
+
+var months = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 
+'Octubre', 'Noviembre', 'Diciembre'];
+months.shift();
+months.pop();
+
+//console.log(months);

2b0fec605eb09bc5989af14d0563f65c4732fd63
FedericoB <federicob@gpl.com.ar>
2020-10-27 21:31:16 -0300

    ejercicio 3 C

---
+++ 3c)/ej3c.js
@@ -0,0 +1,8 @@
+/* Agregar un elemento al principio y al final del array (utilizar unshift y push) */
+
+var months = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 
+'Octubre', 'Noviembre', 'Diciembre'];
+months.unshift('PrimerMes');
+months.push('UltimoMes');
+
+//console.log(months)

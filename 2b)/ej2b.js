88f4287a7c13be77c7cb94fd7d83bf779e9aba77
FedericoB <federicob@gpl.com.ar>
2020-10-27 21:08:12 -0300

    ejercicio 2 B

---
+++ 2b)/ej2b.js
@@ -0,0 +1,8 @@
+/* Crear una variable de tipo string con al menos 10 caracteres y generar 
+un nuevo string con los primeros 5 caracteres guardando el resultado 
+en una nueva variable (utilizar substring). */
+
+var str = 'this string will be cutted';
+var subStrOfStr = str.substring(0, 5);
+
+//console.log(subStrOfStr)

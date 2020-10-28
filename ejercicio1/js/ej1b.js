9bc654b5ab055da97d67a67a74c8c196588faa84
FedericoB <federicob@gpl.com.ar>
2020-10-27 23:02:22 -0300

    ejercicio 1

---
+++ ejercicio1/js/ej1b.js
@@ -0,0 +1,7 @@
+/* Crear dos variables de tipo String y concatenarlas guardando el resultado en una 3er variable. */
+
+var str1 = 'Primer String';
+var str2 = 'Segunda String';
+var concat = str1 + str2;
+
+//console.log(concat)

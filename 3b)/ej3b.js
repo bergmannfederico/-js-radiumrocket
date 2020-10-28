11443aea9ff3ae703cb83b0d5d961f4598063214
FedericoB <federicob@gpl.com.ar>
2020-10-27 21:31:11 -0300

    ejercicio 3 B

---
+++ 3b)/ej3b.js
@@ -0,0 +1,6 @@
+/* Ordenar el array de meses alfabéticamente y mostrarlo por consola (utilizar sort). */
+
+var months = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 
+'Octubre', 'Noviembre', 'Diciembre'];
+
+console.log(months.sort());
